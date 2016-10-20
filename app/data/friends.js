182ae00c7202e8c1a23f66c733b94bc50347f2c6
Genevieve Conty <gmireillec@Genevieves-MacBook-Pro.local>
2016-10-19 20:40:15 -0400

    populate friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,1 +1,81 @@
 // objects
+
+var friends = [{
+	"name":"Bob",
+	"photo":"https://img.buzzfeed.com/buzzfeed-static/static/2015-10/28/18/enhanced/webdr09/grid-cell-19080-1446069966-11.jpg",
+	"scores":[
+		2,
+	    1,
+	    5,
+	    4,
+	    3,
+	    2,
+	    2,
+	    1,
+	    2,
+	    3
+	    ]
+	}, {
+	"name":"Linda",
+	"photo":"http://i.cdn.turner.com/asfix/repository//8a250ba13ef0a786013f0b463a4d020c/thumbnail_1516175656478335318.jpg",
+	"scores":[
+		3,
+	    4,
+	    3,
+	    5,
+	    3,
+	    4,
+	    4,
+	    3,
+	    2,
+	    4
+	    ]
+	}, {
+	"name":"Gene",
+	"photo":"http://www.ew.com/sites/default/files/styles/tout_image_612x380/public/i/2014/10/02/BOBS-BURGERS.jpg?itok=F4X51_9N",
+	"scores":[
+		3,
+	    5,
+	    4,
+	    5,
+	    4,
+	    3,
+	    5,
+	    2,
+	    4,
+	    5
+	    ]
+	}, {
+	"name":"Tina",
+	"photo":"http://pixel.nymag.com/imgs/daily/vulture/2016/02/12/recaps/12-bobs-burgers.w529.h352.jpg",
+	"scores":[
+		2,
+	    5,
+	    3,
+	    5,
+	    2,
+	    1,
+	    1,
+	    5,
+	    2,
+	    4
+	    ]
+	}, {
+	"name":"Louise",
+	"photo":"https://s-media-cache-ak0.pinimg.com/originals/c2/9e/4a/c29e4a1f9ba020549932c9e48f12f7fb.jpg",
+	"scores":[
+		5,
+	    3,
+	    3,
+	    5,
+	    4,
+	    5,
+	    5,
+	    2,
+	    1,
+	    5
+	    ]
+	}
+]
+
+module.exports = friends; 
