26b8e5ce2794372b7bbcfd9132ea633f31f886dd
Genevieve Conty <gmireillec@Genevieves-MacBook-Pro.local>
2016-10-20 01:20:38 -0400

    api friends works

--- server.js
+++ server.js
@@ -14,5 +14,5 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
-require('./app/routing/api-routes.js')(app); 
+require('./app/routing/apiRoutes.js')(app); 
 require('./app/routing/html-routes.js')(app);
