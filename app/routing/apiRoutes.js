7788d8458d682568ff5ceea7cac83b1c5883fc29
Genevieve Conty <gmireillec@Genevieves-MacBook-Pro.local>
2016-10-20 01:17:40 -0400

    html roroutes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,2 +1,28 @@
-//A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
-//A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+//A GET route with the url /api/friends. 
+//This will be used to display a JSON 
+//of all possible friends.
+
+//A POST routes /api/friends. 
+//This will be used to handle incoming 
+//survey results. 
+//handle the compatibility logic.
+
+var daOptions = require('../data/friends.js');
+
+module.exports = function(app){
+	app.get('/api/friends', function (req, res) {
+		res.json(daOptions);
+	});
+
+	// app.post('/api/tables', function (req, res) {
+	// 	var newReservation = req.body;
+	// 	newReservation.routeName = newReservation.customerName.replace(/\s+/g, '').toLowerCase();
+
+	// 	console.log(newReservation);
+
+	// 	reservation.push(newReservation);
+
+	// 	res.json(newReservation);
+	// });
+}
+
